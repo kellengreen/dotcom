bf990af0d4d549e5c350b9165c795563deeb09bd
Kellen Green <me@kellengreen.com>
2014-12-16 19:16:05 -0800

    copy update

--- static/script/squares.js
+++ static/script/squares.js
@@ -27,39 +27,38 @@
         }
 
         var container = document.querySelector('#squares'),
-            count = Math.round(window.innerWidth / 15),
+            count = Math.round(window.innerWidth / 12),
             hideHeight = 30,
             minScale = .5,
             maxScale = 1,
-            minRotate = 1,
-            maxRotate = 1080,
-            minDuratrion = 5,
-            maxDuration = 10,
+            minRotate = 500,
+            maxRotate = 1000,
+            minDuration = 6,
+            maxDuration = 20,
             colors = ['red', 'blue', 'green', 'yellow'];
 
         for (var i = 0; i < count; i++) {
 
-            var elem = document.createElement('div');
-            elem.className = 'square ' + colors[randIntBetween(0, 3)];
+            var elem = document.createElement('div'),
+                translateX = window.innerWidth * Math.random(),
+                translateYStart = -hideHeight,
+                translateYEnd =  window.innerHeight + hideHeight,
+                rotateX = Math.random(),
+                rotateY = Math.random(),
+                rotateZ = Math.random(),
+                rotateA = randIntBetween(minRotate, maxRotate),
+                scale = randFloatBetween(minScale, maxScale),
+                duration = randFloatBetween(minDuration, maxDuration) * 1000,
+                color = colors[randIntBetween(0, 3)],
+                startTransform = 'translate3d(' + translateX + 'px, ' + translateYStart + 'px, 0) ' +
+                    'rotate3d(0, 0, 0, 0deg)' +
+                    'scale(' + scale + ')',
+                endTransform = 'translate3d(' + translateX + 'px, ' + translateYEnd + 'px, 0) ' +
+                    'rotate3d(' + rotateX + ',' + rotateY + ',' + rotateZ + ', ' + rotateA + 'deg)' +
+                    'scale(' + scale + ')';
 
-            var translateX = window.innerWidth * Math.random();
-            var translateYStart = -hideHeight;
-            var translateYEnd =  window.innerHeight + hideHeight;
-            var rotateStart = randIntBetween(minRotate, maxRotate);
-            var rotateEnd = randIntBetween(minRotate, maxRotate);
-            var scale = randFloatBetween(minScale, maxScale);
-            var duration = randFloatBetween(minDuratrion, maxDuration) * 1000;
-
-            var startTransform =
-                'translate3d(' + translateX + 'px, ' + translateYStart + 'px, 0) ' +
-                'rotate3d(1, 1, 1, ' + rotateStart + 'deg)' +
-                'scale(' + scale + ')';
-
-            var endTransform =
-                'translate3d(' + translateX + 'px, ' + translateYEnd + 'px, 0) ' +
-                'rotate3d(1, 1, 1, ' + rotateEnd + 'deg)' +
-                'scale(' + scale + ')';
-
+            elem.classList.add('square');
+            elem.classList.add(color);
             elem.animate([
                 {transform: startTransform},
                 {transform: endTransform}
@@ -67,7 +66,6 @@
                 duration: duration,
                 iterations: Infinity
             });
-
             container.appendChild(elem);
         }
     });
