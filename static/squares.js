2d709385aa22caa4ad42e6bb7cd662ccfc77d993
Kellen Green <me@kellengreen.com>
2016-10-03 13:42:36 +0100

    ES6 update

--- static/squares.js
+++ static/squares.js
@@ -7,57 +7,58 @@ class SquareElem {
      /**
       * SquareElem
       */
-     constructor() {
-         this.elem = document.createElement('div');
-         this.elem.classList.add(SquareElem.baseClass);
-         this.elem.classList.add(SquareElem.colorClasses[SquareElem.randInt(0, SquareElem.colorClasses.length - 1)]);
-     }
+    constructor() {
+        this.elem = document.createElement('div');
+        this.elem.classList.add(SquareElem.baseClass);
+        this.elem.classList.add(SquareElem.colorClasses[SquareElem.randInt(0, SquareElem.colorClasses.length - 1)]);
+    }
 
-     static randFloat(min, max) {
-         return Math.random() * (max - min) + min;
-     }
+    static get overscan() { return 30 };
+    static get minScale() { return .5 };
+    static get maxScale() { return 1 };
+    static get minRotate() { return 500 };
+    static get maxRotate() { return 1000 };
+    static get minDuration() { return 6000 };
+    static get maxDuration() { return 20000 };
+    static get baseClass() { return 'square' };
+    static get colorClasses() { return ['red', 'blue', 'green', 'yellow'] };
 
-     static randInt(min, max) {
-         return Math.floor(Math.random() * (max - min + 1)) + min;
-     }
+    static randFloat(min, max) {
+        return Math.random() * (max - min) + min;
+    }
 
-     fallingAnimation(windowWidth, windowHeight) {
-         const rotateX = Math.random(),
-               rotateY = Math.random(),
-               rotateZ = Math.random(),
-               rotateA = SquareElem.randFloat(SquareElem.minRotate, SquareElem.maxRotate),
-               scale = SquareElem.randFloat(SquareElem.minScale, SquareElem.maxScale),
-               translateX = windowWidth * Math.random(),
-               translateY = windowHeight + SquareElem.overscan,
-               duration = SquareElem.randFloat(SquareElem.minDuration, SquareElem.maxDuration),
-               transformStart = `translate(${translateX}px, -${SquareElem.overscan}px) scale(${scale}) rotate3d(0, 0, 0, 0deg)`,
-               transformEnd =   `translate(${translateX}px, ${translateY}px) scale(${scale}) rotate3d(${rotateX}, ${rotateY}, ${rotateZ}, ${rotateA}deg)`;
-         return this.elem.animate([
-             {transform: transformStart, opacity: 1},
-             {transform: transformEnd, opacity: 1},
-         ], {
-             duration: duration,
-             iterations: Infinity,
-         });
-     }
+    static randInt(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
 
-     fadeAnimation() {
+    fallingAnimation(windowWidth, windowHeight) {
+        const rotateX = Math.random();
+        const rotateY = Math.random();
+        const rotateZ = Math.random();
+        const rotateA = SquareElem.randFloat(SquareElem.minRotate, SquareElem.maxRotate);
+        const scale = SquareElem.randFloat(SquareElem.minScale, SquareElem.maxScale);
+        const translateX = windowWidth * Math.random();
+        const translateY = windowHeight + SquareElem.overscan;
+        const duration = SquareElem.randFloat(SquareElem.minDuration, SquareElem.maxDuration);
+        const transformStart = `translate(${translateX}px, -${SquareElem.overscan}px) scale(${scale}) rotate3d(0, 0, 0, 0deg)`;
+        const transformEnd =   `translate(${translateX}px, ${translateY}px) scale(${scale}) rotate3d(${rotateX}, ${rotateY}, ${rotateZ}, ${rotateA}deg)`;
+
         return this.elem.animate([
-             {opacity: 1},
-             {opacity: 0},
-        ], {duration: 1000});
-     }
-}
+            {transform: transformStart, opacity: 1},
+            {transform: transformEnd, opacity: 1},
+        ], {
+            duration: duration,
+            iterations: Infinity,
+        });
+    }
 
-SquareElem.overscan = 30;
-SquareElem.minScale = .5;
-SquareElem.maxScale = 1;
-SquareElem.minRotate = 500;
-SquareElem.maxRotate = 1000;
-SquareElem.minDuration = 6000;
-SquareElem.maxDuration = 20000;
-SquareElem.baseClass = 'square';
-SquareElem.colorClasses = ['red', 'blue', 'green', 'yellow'];
+    fadeAnimation() {
+        return this.elem.animate([
+            {opacity: 1},
+            {opacity: 0},
+        ], {duration: 1000});
+    }
+}
 
 class SquareContainer {
     /**
@@ -71,6 +72,9 @@ class SquareContainer {
         this.initAnimations();
         window.addEventListener('resize', this.debounce(this.resize, 100));
     }
+
+    static get pxPerSquare() { return 12 };
+    static get baseClass() { return 'squares' };
 
     debounce(fn, delay) {
         let timeout;
@@ -108,8 +112,5 @@ class SquareContainer {
         }
     }
 }
-
-SquareContainer.pxPerSquare = 12;
-SquareContainer.baseClass = 'squares';
 
 new SquareContainer();
